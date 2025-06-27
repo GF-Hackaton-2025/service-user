475b547a49548b790463c98f1ca336555e5bead9
Gustavo Biasi <>
2025-06-26 22:11:38 -0300

    feat: update OpenApiConfig paths and enhance LoginResponse with access token and user type descriptions

--- src/main/java/br/com/user/config/JwtConfig.java
+++ src/main/java/br/com/user/config/JwtConfig.java
@@ -4,7 +4,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 
 public class JwtConfig {
 
-  public static final String issuer = "service-user";
+  public static final String issuer = "service";
   public static final Algorithm algorithm = Algorithm.HMAC256("F5Mc7x1nBDhfq2pI4AZc6fGOlIZEVDCOvc2I");
 
 }
